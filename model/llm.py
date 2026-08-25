fa755e3007c55f61216e44018a78847d295cbc52
cavsang <cavsang@naver.com>
2026-08-25 18:09:37 +0900

    okok

--- model/llm.py
+++ model/llm.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # API 키 설정
 if not os.getenv("OPENAI_API_KEY"):
-    os.environ["OPENAI_API_KEY"] = "password"
+    os.environ["OPENAI_API_KEY"] = "password"   
 
 
 def get_llm() ->  Optional[BaseChatModel]:
