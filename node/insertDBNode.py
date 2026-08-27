0bf6df4a14a1462d7e18628834f624bee0de195a
cavsang <cavsang@naver.com>
2026-08-27 11:11:47 +0900

    .

--- node/insertDBNode.py
+++ node/insertDBNode.py
@@ -6,6 +6,6 @@ from schema.schema import ProblemGenerationState
 def insertDBNode(state: ProblemGenerationState) -> dict:
     """문제를 DB에 저장한다."""
 
-    print(state)
+    print(state.generated_problem)
     
     return {}
